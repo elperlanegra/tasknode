654bc48ce2d42a35b2a0612da0f587771fa44000
Manuel <carlosd99m@gmail.com>
2021-03-06 01:20:11 -0600

    instalando pug

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 
 //crear la aplicación express
 const app = express();
 
+// Habilitar pug
+app.set('view engine', 'pug');
+
+// Añadir la carpeta de las vistas
+app.set('views', path.join(__dirname, './views'));
+
+
 //rutas 
 app.use('/', routes() );
 
-app.listen(3000)
+app.listen(3000);
+console.log("Servidor en linea en http://localhost:3000");
